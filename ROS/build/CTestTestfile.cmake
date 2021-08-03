aa7dddcca64661d30e05e6963c926b86ccbeb4f2
suveergarg <gargsuveer@gmail.com>
2021-08-03 17:43:47 -0400

    value iteration

--- ROS/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/suveer/Desktop/AI/ROS/src
-# Build directory: /home/suveer/Desktop/AI/ROS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
